ec85a1100af64d4eeecf50d575d7813b7a0d0176
Tomas Kypta <147535054+tokypt@users.noreply.github.com>
2023-11-29 10:22:59 +0100

    Update engeto_data_course_2023_kypta_4.sql

--- engeto_data_course_2023_kypta_4.sql
+++ engeto_data_course_2023_kypta_4.sql
@@ -11,7 +11,6 @@ JOIN czechia_price_category
  * že vhodně přejmenujete tabulky a vypíšete 
  * ID a jméno kategorie potravin a cenu.
  */
-
 SELECT 
 	cp.id, cpc.name, cp.value
 FROM czechia_price AS cp
@@ -80,7 +79,6 @@ LIMIT 1;
  * celé názvy odvětví a kategorií potravin a datumy měření, 
  * které vhodně naformátujete.
  */
-
 SELECT cpc.name AS food_category,cp.value AS price 
 ,cpib.name AS industry ,cp2.value AS average_wage
 ,cp2.payroll_year , YEAR(cp.date_from)
@@ -95,11 +93,50 @@ JOIN czechia_price_category AS cpc
 	ON cpc.code = cp.category_code 
 JOIN czechia_payroll_industry_branch AS cpib 
 	ON cpib.code = cp2.industry_branch_code;
-
 -- https://mariadb.com/kb/en/date_format/
 
+/*
+ * Úkol 8: K tabulce healthcare_provider připojte informace 
+ * o regionech a vypište celé názvy krajů i okresů pro místa výkonu i sídla.
+ */
+SELECT 
+	cr.name AS region_name,
+    cr2.name AS residence_region_name,
+    hp.name,
+    cd.name AS district_name,
+    cd2.name AS residence_district_name
+FROM healthcare_provider AS hp 
+LEFT JOIN czechia_region AS cr 
+	ON hp.region_code = cr.code 
+LEFT JOIN czechia_district AS cd 
+	ON hp.district_code = cd.code
+LEFT JOIN czechia_region AS cr2 
+	ON hp.residence_region_code = cr2.code 
+LEFT JOIN czechia_district AS cd2 
+	ON hp.residence_district_code = cd2.code;
 
---- 8 a 9 dodělat 
+/*
+ * Úkol 9: Upravte předchozí dotaz tak, aby byli na výpisu pouze takoví poskytovatelé, 
+ * kteří mají sídlo v jiném kraji i jiném okrese než místo poskytování služeb.
+ */
+SELECT 
+	cr.name AS region_name,
+    cr2.name AS residence_region_name,
+    hp.name,
+    cd.name AS district_name,
+    cd2.name AS residence_district_name
+FROM healthcare_provider AS hp
+LEFT JOIN czechia_region AS cr 
+	ON hp.region_code = cr.code 
+LEFT JOIN czechia_district AS cd 
+	ON hp.district_code = cd.code
+LEFT JOIN czechia_region AS cr2 
+	ON hp.residence_region_code = cr2.code 
+LEFT JOIN czechia_district AS cd2 
+	ON hp.residence_district_code = cd2.code
+WHERE hp.region_code != hp.residence_region_code 
+	AND hp.district_code != hp.residence_district_code;
+
 
 -- Kartézský součin
 /* Úkol 1: Spojte tabulky czechia_price a czechia_price_category 
@@ -205,7 +242,7 @@ WHERE value IN (
  *  které jsou v Jihomoravském kraji jiné na sloupcích category_code 
  * a value než v Praze.
  */
-SELECT category_code, value
+SELECT category_code ,value
 FROM czechia_price
 WHERE region_code = 'CZ064'
 EXCEPT
@@ -246,11 +283,246 @@ INTERSECT
  * cenami nad 150 Kč. S její pomocí následně vypište 
  * jména takových kategorií potravin, které do této cenové hladiny spadají.
  */
+WITH high_price AS (
+	SELECT *
+	FROM czechia_price as cp 
+	WHERE cp.value > 150; 
+)
+SELECT *
+FROM high_price;
+
+-- ekvivalentní dotazy
+SELECT * 
+FROM (
+	SELECT *
+	FROM czechia_price as cp 
+	WHERE cp.value > 150; 
+)
 
 
--- příští lekce
+/*
+ * Úkol 2: Zjistěte, ve kterých okresech mají všichni praktičtí lékaři 
+ * vyplněný telefon, fax, nebo e-mail. Pro tyto účely si připravte dočasnou 
+ * tabulku s výčtem okresů, ve kterých tato podmínka naopak splněna není, 
+ * pod názvem not_completed_provider_info_district.
+ */
+WITH not_completed_provider_info_district AS (
+	SELECT DISTINCT hp.district_code
+	FROM healthcare_provider as hp 
+	WHERE 
+		hp.phone IS NULL
+		AND hp.fax IS NULL
+		AND hp.email IS NULL
+		AND hp.provider_type = 'Samost. ordinace všeob. prakt. lékaře'
+)
+SELECT *
+FROM czechia_district AS cd
+WHERE cd.code NOT IN (
+	SELECT *
+	FROM not_completed_provider_info_district
+	);
 
 
+-- Úkol 3: Vypište z tabulky economies průměr světových daní, při HDP vyšším než 70 miliard.
+WITH large_gdp_area AS (
+	SELECT *
+	FROM economies as e 
+	WHERE e.GDP > 70000000000
+)
+SELECT avg(taxes) AS taxes_avg
+FROM large_gdp_area;
+
+
+-- Countries: JOIN
+/*
+ * Úkol 1: K tabulce countries připojte tabulku religions. 
+ * Vybere název státu, hlavní město, celkovou populaci, název náboženství 
+ * a počet jeho příslušníků. Vyberte pouze rok 2020.
+ */
+SELECT r.country, c.capital_city, c.population, r.religion, r.population
+FROM countries AS c 
+JOIN religions AS r 
+	ON c.country = r.country
+WHERE r.YEAR = 2020;
+
+/*
+ * Úkol 2: K tabulce countries připojte tabulku economies. 
+ * Pro každý stát vyberte hodnoty HDP v milionech dolarů, gini koeficient a daně za období, 
+ * kdy byla země samostatná (independence_date).
+ */
+SELECT c.country, round(e.GDP / 1000000, 2), e.gini, e.taxes, c.independence_date, e.YEAR, e.population
+FROM countries AS c 
+JOIN economies AS e 
+	ON c.country = e.country
+WHERE c.independence_date <= e.YEAR;
+
+/*
+ * Úkol 3: Zjistěte, které země se nacházejí v tabulce countries, 
+ * ale ne v tabulce economies. Seřaďte je sestupně podle počtu obyvatel.
+ */
+SELECT country
+FROM countries
+EXCEPT
+SELECT country
+FROM economies;
+
+SELECT country
+FROM economies AS e 
+WHERE country = 'Christmas Island';
+
+SELECT c.country , c.population
+FROM countries c 
+LEFT JOIN economies e 
+    on c.country = e.country 
+    and e.year=2018 -- vyberu si jenom jeden rok
+WHERE e.country is null
+ORDER BY c.population DESC;
+
+/*
+ * Úkol 4: Joiny můžeme používat nejenom pro spojování dvou různých tabulek. 
+ * Můžeme napojovat i jednu tabulku samu na sebe, abychom z ní zjistili nové informace.
+ * Použijte tabulku life_expectancy abyste pro každý stát zjistili podíl doby dožití v roce 2015 a v roce 1970.
+ */
+SELECT a.country, a.life_exp_2015, b.life_exp_1970, round( a.life_exp_2015 / b.life_exp_1970, 2 ) as life_exp_ratio
+FROM (SELECT country, life_expectancy as life_exp_2015 FROM life_expectancy WHERE year = 2015) AS a
+JOIN (SELECT country, life_expectancy as life_exp_1970 FROM life_expectancy WHERE year = 1970) AS b
+	ON a.country = b.country;
+
+/*
+ * Úkol 5: Z tabulky economies spočítejte meziroční procentní nárůst populace a procentní nárůst HDP pro každou zemi.
+ * Nápověda: Napojte tabulku samu na sebe pomocí klíče year. Ke klíči pravé tabulky přičtěte 1.
+ */
+SELECT e.country 
+	,e.year
+	,e2.year as year_prev
+	,round( ( e.GDP - e2.GDP ) / e2.GDP * 100, 2 ) as GDP_growth
+    ,round( ( e.population - e2.population ) / e2.population * 100, 2) as pop_growth_percent
+FROM economies as e 
+JOIN economies as e2 
+	ON e.country = e2.country
+	AND e.year = e2.year + 1
+	AND e.year < 2020;
+
+SELECT
+    e.country,
+    e.year,
+    e.population,
+    e.GDP,
+    e2.year as next_year,
+    ROUND((e2.population - e.population) / e.population * 100, 2) as pop_growth_percent,
+    ROUND((e2.GDP - e.GDP) / e.GDP * 100, 2) as GDP_growth_percent
+FROM economies as e
+JOIN economies as e2 ON e.country = e2.country AND e.year + 1 = e2.year;
+
+
+/*
+ * Úkol 6: Počty věřících v tabulce religions pro rok 2020 přepočítejte na procentní podíl.
+ * Nápověda: agregujte tabulku podle jednotlivých zemí. Sečtěte počet obyvatel
+ * agregovanou tabulku připojte na původní tabulku
+ */
+SELECT
+	r.country,
+	r.religion,
+	round((r.population / r2.total_population) * 100, 3) AS religion_2020
+FROM
+	religions as r
+JOIN (
+	SELECT
+		country,
+		sum(population) AS total_population
+	FROM
+		religions
+	WHERE
+		year = 2020
+	GROUP BY
+		country) as r2
+	ON
+	r.country = r2.country
+	AND r.year = 2020;
+
+
+-- COVID-19: JOIN
+-- Úkol 1a: Vytvořte view z lookup_table, kde je sloupec provincie null
+CREATE VIEW lookup_table_province AS
+SELECT *
+FROM lookup_table as lt
+WHERE province IS NULL;
+
+
+/*
+ * Úkol 1b: Spojte pomocí left join tabulku covid19_basic 
+ * s view vytvořeným v předchozím úkolu přes country
+ */
+SELECT *
+FROM covid19_basic as cb 
+LEFT JOIN lookup_table_province as ltp 
+	ON cb.country = ltp.country;
+
+-- Úkol 2: Spojte tabulky covid19_basic a covid19_basic_difference pomocí left join
+SELECT *
+FROM covid19_basic as cb 
+LEFT JOIN covid19_basic_differences as cbd 
+	ON cb.country = cbd.country
+	AND cb.`date` = cbd.`date`;
+	
+/* Úkol 3: Spojte tabulky covid19_detail_us a covid19_detail_us_differences 
+ * skrze sloupce date, country, admin2. Z tabulky covid19_detail_us vyberte 
+ * všechny sloupce a tabulky covid19_detail_us_differences jen confirmed a 
+ * přejmenujte ho na confirmed_diff. Pro spojení použijte left join
+ */
+SELECT cdu.*, cdud.confirmed AS confirmed_diff
+FROM covid19_detail_us as cdu 
+LEFT JOIN covid19_detail_us_differences as cdud 
+	ON cdu.date = cdud.date
+	AND cdu.country = cdud.country
+	AND cdu.province = cdud.province
+	AND cdu.admin2 = cdud.admin2;
+
+/*
+ * Úkol 4: Spojte pomocí left join tabulky covid19_detail_us, 
+ * covid19_detail_us_differences a lookup_table
+ */
+SELECT *
+FROM covid19_detail_us as cdu 
+LEFT JOIN covid19_detail_us_differences as cdud 
+	ON cdu.date = cdud.date
+	AND cdu.country = cdud.country
+	AND cdu.province = cdud.province
+	AND cdu.admin2 = cdud.admin2
+LEFT JOIN lookup_table as lt 
+	ON cdu.country = lt.country
+	AND cdu.province = lt.province
+	AND cdu.admin2 = lt.admin2;
+
+/*
+ * Úkol 5: Spojte pomocí left join tabulky 
+ * covid19_detail_global a covid19_detail_global_differences
+ */
+SELECT *
+FROM covid19_detail_global as cdg 
+LEFT JOIN covid19_detail_global_differences as cdgd 
+	ON cdg.date = cdgd.date
+	AND cdg.country = cdgd.country
+	AND cdg.province = cdgd.province;
+
+/*
+ * Úkol 6: Spojte pomocí left join tabulky covid19_detail_global, 
+ * covid19_detail_global_differences a lookup_table
+ */
+SELECT *
+FROM covid19_detail_global as cdg 
+LEFT JOIN covid19_detail_global_differences as cdgd 
+	ON cdg.date = cdgd.date
+	AND cdg.country = cdgd.country
+	AND cdg.province = cdgd.province
+LEFT JOIN lookup_table as lt 
+	ON cdg.country = lt.country
+	AND cdg.province = lt.province;
+
+/*
+ * Úkol 7: Jaký je průbeh počtu nakažených na milion obyvatel
+ * v Česke republice a v Německu
+ */
 
 
 
